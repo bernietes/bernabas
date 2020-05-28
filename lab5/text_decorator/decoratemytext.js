64c55f21c8d0d5d8944b50a767ea99314a086b98
Grace <bernietes0@gmail.com>
2020-05-27 21:40:48 -0500

    Lab 5 Assignment

---
+++ lab5/text_decorator/decoratemytext.js
@@ -0,0 +1,24 @@
+
+function big_decoration(){
+    let textbox = document.getElementById("text_area");
+    textbox.style.fontSize = parseInt(textbox.style.fontSize) + 2 + "pt";
+
+}
+
+
+function bling() {
+
+    var checkbox=document.getElementById('bling')
+    var textarea= document.getElementById('text_area');
+    if (checkbox.checked) {
+        textarea.style.fontWeight = 'bold';
+        textarea.style.color = 'green';
+        textarea.style.textDecoration = 'underline';
+    }
+    else{
+        textarea.style.fontWeight='normal';
+        textarea.style.color='black';
+        textarea.style.textDecoration='none';
+}
+
+}
